c13d59b92c3126ecdca60eefe95ca44f38d14ba9
huimingdeng <13570313864@163.com>
2018-08-24 16:59:19 +0800

    create a new plugin BmnarsAudit

---
+++ BmnarsAudit/bmnarsaudit.php
@@ -0,0 +1,31 @@
+<?php 
+/**
+ * Plugin Name: Bmnars Audit
+ * Plugin URI: https://github.com/huimingdeng/genecopoeia/BmnarsAudit
+ * Description: 生命的奥秘——爬虫文章审核发布插件。
+ * Version: 0.0.1
+ * Author: DHM (huimingdeng)
+ * Author URI: https://github.com/huimingdeng/
+ */
+global $wpdb;
+
+function add_Bmnars_menu(){
+	add_menu_page(
+      'BmnarsAudit',
+      'Bmnars Audit',
+      'edit_pages',
+      'BmnarsAudit',
+      'render_bmnars_list_page'
+    );
+}
+
+function render_bmnars_list_page(){
+	include 'Views/bmnars-list.php';
+}
+
+add_action('admin_menu','add_Bmnars_menu');
+
+function Bmnars_Audit_init() {
+    load_plugin_textdomain( 'BmnarsAudit', false, dirname( plugin_basename( __FILE__ ) ) );
+}
+add_action( 'plugins_loaded', 'Bmnars_Audit_init' );
