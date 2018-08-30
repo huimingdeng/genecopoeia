8c7bae74fa5b86565ff707ada3f21dd2f95a55a9
huimingdeng <13570313864@163.com>
2018-08-30 10:00:19 +0800

    add a plugin in genecopoeia.git

---
+++ SearchMenuOptions/README.md
@@ -0,0 +1,6 @@
+# SearchMenuOptions plugin #
+genecopoeia 网站搜索页面的克隆产品筛选菜单管理插件。（尝试插件开发使用面向对象，摈弃前面面向过程的形式）
+
+version description：
+
+v0.0.1 设计版本：设计页面结构，1.直接管理数据表 or 2.界面化操作按产品种类划分。
---
+++ SearchMenuOptions/searchmenuoptions.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * Plugin Name: Search Menu Options
+ * Plugin URI: https://github.com/huimingdeng/genecopoeia/tree/master/SearchMenuOptions
+ * Description: genecopoeia 网站搜索页面的克隆产品筛选菜单管理插件。
+ * Version: 0.0.1
+ * Author: DHM (huimingdeng)
+ * Author URI: #
+ * License: Open source but the copyright is used by genecopoeia.
+ */
+
+class SearchMenuOptions{
+
+}
