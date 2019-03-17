0d1ab5a54ba1a229d1f46cb65b37aa064d226adf
huimingdeng <13570313864@163.com>
2019-03-17 17:30:24 +0800

    添加模型类格式实现，判断用户字符类型

--- MyFAQs/classes/Model.php
+++ MyFAQs/classes/Model.php
@@ -17,6 +17,7 @@ class Model
     private $sql;
     private $fields = '*';
     private $limited;
+    private $msg = array();
 
     public function __construct($tab_name)
     {
@@ -46,8 +47,108 @@ class Model
      * @return bool true|false;
      */
     public function addOne($data){
+        $data['pubdate'] = $data['editdate'] = date('Y-m-d H:i:s',time());
+        $fields = array_keys($data);
+        foreach ($data as $k => $v)
+        {
+            $data[$k] = $this->format($k , $v);
+//            var_dump($data[$k]);
+        }
+//        var_dump($data);
+        $values = array_values($data);
+        // 需要按照table的字段类型设置字段
+        // ...
+        $query = sprintf("INSERT INTO {$this->table} (%s) VALUES(%s)",implode(',', $fields),implode(',',$values));
+        $bool = $this->wpdb->query($query);
 
-        return true;
+        if($bool !== false){
+            $this->msg = array( 'status'=>200, 'msg'=>_('Data added successfully','myfaqs'));
+        }else{
+            $this->msg = array( 'status'=>500, 'msg'=>_('Data addition error.','myfaqs'));
+        }
+
+        return $this->msg;
+    }
+
+    public function query($sql = ''){
+        $data = $this->wpdb->get_results($sql, ARRAY_A);
+        return $data;
+    }
+
+    public function getConstucture(){
+        // Get table structure
+        $sql  = 'describe ' . $this->table;
+        $data = $this->query($sql);
+
+        $constucture = array();
+
+        foreach ($data as $v)
+        {
+            $constucture[$v['Field']] = $this->getType($v['Type']);
+        }
+
+        return $constucture;
+    }
+
+    /**
+     * Gets the concrete data type
+     * @param string $type
+     * @return string
+     */
+    public function getType($type = ''){
+        if (preg_match("/tinyint/" , $type) === 1) {
+            return 'tinyint';
+        }
+
+        if (preg_match("/int/" , $type) === 1) {
+            return 'int';
+        }
+
+        if (preg_match("/char/" , $type) === 1) {
+            return 'char';
+        }
+
+        if (preg_match("/varchar/" , $type) === 1) {
+            return 'varchar';
+        }
+
+        if (preg_match("/text/" , $type) === 1) {
+            return 'text';
+        }
+
+        if (preg_match('/datetime/', $type) === 1){
+            return 'datetime';
+        }
+        // 数据库类型不止以上这些，其他的请自行补充
+        // ...
+    }
+
+    /**
+     * Formatting strings
+     * @param string $key
+     * @param string $val
+     * @return bool|string
+     */
+    public function format($key='', $val=''){
+
+        $group = $this->getConstucture();
+        $add_quote_type_range = array('char' , 'varchar' , 'text', 'datetime');
+
+        foreach ($group as $k => $type)
+        {
+            if ($k === $key) {
+                foreach ($add_quote_type_range as $ok_type)
+                {
+                    if ($type === $ok_type){
+                        return "'" . $val . "'";
+                    }
+                }
+
+                return $val;
+            }
+        }
+
+        return false;
     }
 
     public function limitpage($start=0,$offeser=20){
