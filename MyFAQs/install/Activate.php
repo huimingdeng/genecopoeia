6d44445d42f5e7ec30d645d63d57c830061ba447
huimingdeng <13570313864@163.com>
2019-03-26 14:13:16 +0800

    修改插件安装激活和卸载类的实现

--- MyFAQs/install/Activate.php
+++ MyFAQs/install/Activate.php
@@ -12,12 +12,12 @@ namespace MyFAQs\Install;
 class Activate
 {
     public function plugin_active($networ = FALSE){
-        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-        $category =  file_get_contents(dirname(dirname(__FILE__)).'/Sql/_faq_categories.sql');
-        dbDelta( $category ,true);
-        $faq = file_get_contents(dirname(dirname(__FILE__)).'/Sql/_faq_question.sql');
-        dbDelta( $faq, true);
-        $shortcode = file_get_contents(dirname(dirname(__FILE__)).'/Sql/_faq_shortcode.sql');
-        dbDelta( $shortcode, true);
+        // require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        // $category =  file_get_contents(dirname(dirname(__FILE__)).'/Sql/_faq_categories.sql');
+        // dbDelta( $category ,true);
+        // $faq = file_get_contents(dirname(dirname(__FILE__)).'/Sql/_faq_question.sql');
+        // dbDelta( $faq, true);
+        // $shortcode = file_get_contents(dirname(dirname(__FILE__)).'/Sql/_faq_shortcode.sql');
+        // dbDelta( $shortcode, true);
     }
 }
--- MyFAQs/install/Deactivate.php
+++ MyFAQs/install/Deactivate.php
@@ -11,5 +11,39 @@ namespace MyFAQs\Install;
 
 class Deactivate
 {
-    
+	/**
+	 * The plugin uninstall performs the delete table operation 
+	 */
+    public function plugin_deactivation(){
+    	$this->remove_database_tables();
+    }
+
+    /**
+     * Drops all database tables known to MyFAQs.
+     */
+    protected function remove_database_tables()
+	{
+		global $wpdb;
+
+		$tables = $this->get_table_data();
+		foreach ($tables as $table) {
+			$sql = "DROP TABLE IF EXISTS `_faq_{$table}`";
+			$wpdb->query($sql);
+		}
+	}
+
+	/**
+	 * Returns array containing information on database tables
+	 * @return array Database information
+	 */
+	protected function get_table_data()
+	{
+		$tables = array(
+			'question', // Because of the foreign key relationship, the `question` table must be dropped first.
+			'categories',
+			'shortcode',
+		);
+
+		return $tables;
+	}
 }
