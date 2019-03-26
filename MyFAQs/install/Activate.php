545f040fe2e2bd4ed38d42fff5edda96f3803346
huimingdeng <13570313864@163.com>
2019-03-26 15:26:42 +0800

    修改实现激活插件则创建数据表

--- MyFAQs/install/Activate.php
+++ MyFAQs/install/Activate.php
@@ -12,12 +12,86 @@ namespace MyFAQs\Install;
 class Activate
 {
     public function plugin_active($networ = FALSE){
-        // require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-        // $category =  file_get_contents(dirname(dirname(__FILE__)).'/Sql/_faq_categories.sql');
-        // dbDelta( $category ,true);
-        // $faq = file_get_contents(dirname(dirname(__FILE__)).'/Sql/_faq_question.sql');
-        // dbDelta( $faq, true);
-        // $shortcode = file_get_contents(dirname(dirname(__FILE__)).'/Sql/_faq_shortcode.sql');
-        // dbDelta( $shortcode, true);
+       $this->create_database_tables();
+    }
+
+    /**
+     * @return array
+     */
+    protected function get_table_data()
+    {
+        $ret = array(
+            'categories' =>
+                "CREATE TABLE `categories` (
+                  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+                  `name` varchar(255) NOT NULL COMMENT '分类名',
+                  `slug` varchar(255) NOT NULL COMMENT '别名,必须英文',
+                  `pubdate` datetime NOT NULL COMMENT '发布时间',
+                  `editdate` datetime NOT NULL COMMENT '修改时间',
+                  `sumfaq` int(10) unsigned NOT NULL COMMENT '统计当前分类faq数量',
+                  `parent` int(10) DEFAULT NULL COMMENT '父级分类',
+                  PRIMARY KEY (`id`),
+                  UNIQUE KEY `name` (`name`)
+                ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;",
+            'question' =>
+                "CREATE TABLE `question` (
+                  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                  `title` varchar(255) NOT NULL COMMENT 'issue',
+                  `answer` text NOT NULL COMMENT 'answer',
+                  `pubdate` datetime NOT NULL,
+                  `editdate` datetime NOT NULL,
+                  `category` int(10) unsigned DEFAULT NULL,
+                  PRIMARY KEY (`id`),
+                  UNIQUE KEY `issue` (`title`),
+                  KEY `catagory_id` (`category`),
+                  CONSTRAINT `catagory_id` FOREIGN KEY (`category`) REFERENCES `_faq_categories` (`id`) ON DELETE CASCADE
+                ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;",
+            'shortcode' =>
+                "CREATE TABLE `shortcode` (
+                  `id` int(11) NOT NULL AUTO_INCREMENT,
+                  `short_code` varchar(50) DEFAULT NULL,
+                  `location` varchar(100) DEFAULT NULL COMMENT '记录使用的wp_posts表的ID',
+                  `pubdate` datetime NOT NULL,
+                  `editdate` datetime NOT NULL,
+                  PRIMARY KEY (`id`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8;",
+        );
+        return $ret;
+    }
+
+    /**
+     * create tables.
+     */
+    protected function create_database_tables()
+    {
+        global $wpdb;
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+        // Hack to reduce errors. Removing backticks makes the dbDelta function work better. #164
+        add_filter('dbdelta_create_queries', array($this, 'filter_dbdelta_queries'));
+        add_filter('wp_should_upgrade_global_tables', '__return_true'); // The new version of the plugin updates information
+
+        $errors = $wpdb->show_errors(FALSE);				// disable errors #164
+        $tables = $this->get_table_data();
+        foreach ($tables as $table => $sql) {
+            $sql = str_replace('CREATE TABLE `', 'CREATE TABLE `' . '_faq_', $sql);
+            $ret = dbDelta($sql);
+        }
+
+        $wpdb->show_errors($errors);						// reset errors #164
+
+    }
+
+    /**
+     * @param $c_queries
+     * @return array
+     */
+    public function filter_dbdelta_queries($c_queries)
+    {
+        $ret_queries = array();
+        foreach ($c_queries as $query) {
+            $ret_queries[] = str_replace('`', '', $query);
+        }
+        return $ret_queries;
     }
 }
--- MyFAQs/myfaqs.php
+++ MyFAQs/myfaqs.php
@@ -4,7 +4,7 @@
  * Plugin URI: #
  * Description: FAQ 展示工具，后期将结合百度 AnyQ | RasaHQ 形成机器客服
  * Author: DHM(huimingdeng)
- * Version: 0.0.12
+ * Version: 0.0.13
  */
 namespace MyFAQs;
 
@@ -15,7 +15,7 @@ use MyFAQs\Install\Deactivate;
 
 class MyFAQs{
     private static $_instance = null;
-    const VERSION = '0.0.12';
+    const VERSION = '0.0.13';
     const PLUGIN_NAME = 'MyFAQs';
     /**
      * Register to load class files, 
