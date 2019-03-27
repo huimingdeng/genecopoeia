bae097c4694dac4359e213fea217f4bac0b6c4c9
huimingdeng <13570313864@163.com>
2019-03-27 17:18:03 +0800

    DIVI 主题能使用短代码，旧插件需要修改调试

--- MyFAQs/views/shortcode.php
+++ MyFAQs/views/shortcode.php
@@ -1,4 +1,29 @@
+<link rel="stylesheet" href="<?php echo dirname(plugin_dir_url(__FILE__)). '/assets/css/bootstrap.min.css';?>" type="text/css">
 <h1>hello</h1>
 <div class="wrap">
-	<?php print_r($atts); ?>
+	<?php global $wpdb;
+		$faq = $wpdb->get_row("SELECT * FROM _faq_question", ARRAY_A);
+	 ?>
+	<table class="table table-striped">
+		<thead>
+	        <tr>
+	            <th ><input type="checkbox" name="select"></th>
+	            <th width="15%"><?php _e('Title','myfaqs');?></th>
+	            <th width="40%"><?php _e('Answer', 'myfaqs');?></th>
+	            <th><?php _e('Category', 'myfaqs');?></th>
+	            <th><?php _e('EditDate', 'myfaqs');?></th>
+	            <th><?php _e('Action', 'myfaqs'); ?></th>
+	        </tr>
+        </thead>
+        <tbody>
+        	<tr>
+        		<td></td>
+        		<td><?php echo $faq['title']; ?></td>
+        		<td><?php echo $faq['answer']; ?></td>
+        		<td><?php echo $faq['category']; ?></td>
+        		<td><?php echo $faq['editdate']; ?></td>
+        		<td><?php echo $faq['id']; ?></td>
+        	</tr>
+        </tbody>
+	</table>
 </div>
