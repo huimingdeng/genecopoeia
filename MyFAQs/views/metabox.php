23ecc3af010f39068552c069c71fb47013d37fe9
huimingdeng <13570313864@163.com>
2019-04-04 16:54:42 +0800

    metabox页面样式设计

--- MyFAQs/views/metabox.php
+++ MyFAQs/views/metabox.php
@@ -1,7 +1,16 @@
-
-<div>
+<style type="text/css">
+	.metabox{width:100%; clear: both;}
+	.metabox ul { width: 100%; border: 1px solid #333;}
+	.metabox ul li {width: 100%; margin:2px 5px; list-style: none; display: block; line-height: 30px;}
+	.metabox ul li span{display: inline-block; margin-right: 5px; width:auto; text-align: center;}
+	.metabox ul li.meta-title>span:first-of-type{ width:10%; }
+	.metabox ul li.meta-title>span:last-of-type{ width:70%; }
+</style>
+<div class="metabox">
 	<ul>
-		<li><span><input type="checkbox" name="select"></span><span>question</span></li>
-		<li></li>
+		<li class="meta-title"><span><input type="checkbox" name="select"></span><span>question</span></li>
+		<li class="meta-body">
+			
+		</li>
 	</ul>
 </div>
