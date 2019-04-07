8c7ed8bd3bbef58a5a67fcf219270953a0920753
huimingdeng <13570313864@163.com>
2019-04-07 18:16:45 +0800

    增加保存和关闭/清除按钮

--- MyFAQs/views/faqlist.php
+++ MyFAQs/views/faqlist.php
@@ -1,5 +1,9 @@
+<div class="btn-list">
+	<input type="button" class="btn btn-save" onclick="Traces.save();" value="<?php _e('Save', 'myfaqs'); ?>">
+	<input type="button" class="btn btn-close" onclick="Traces.close();" value="<?php _e('Close', 'myfaqs'); ?>">
+</div>
 <ul class="bd">
-		<li class="meta-title"><span class="box"><input type="checkbox" class="select"></span><span class="tit"><?php _e('Title', 'myfaqs'); ?></span><span class="ac"><?php _e('Action', 'myfaqs'); ?></span></li>
+	<li class="meta-title"><span class="box"><input type="checkbox" class="select"></span><span class="tit"><?php _e('Title', 'myfaqs'); ?></span><span class="ac"><?php _e('Action', 'myfaqs'); ?></span></li>
 <?php foreach($faqs as $tit){ ?>
     <li class="meta-body">
         <span><input type="checkbox" name="ids[]" value="<?php echo $tit['id'];?>"></span>
