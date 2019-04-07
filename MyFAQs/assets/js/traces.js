36a18b16b23da3a57e65c72aaafcedfa686c39dc
huimingdeng <13570313864@163.com>
2019-04-07 17:52:45 +0800

    实现异步返回faq列表，用于设置短代码信息

--- MyFAQs/assets/js/traces.js
+++ MyFAQs/assets/js/traces.js
@@ -7,9 +7,36 @@ function Traces() {
     this.operation = 'init';
     this.ajaxponit = 'myfaqs';
 }
-
+// 用于查询当前类型的短代码数量并返回信息
 Traces.prototype.init = function() {
 
+};
+
+Traces.prototype.list = function() {
+    this.operation = 'popup';
+    var _self = this;
+    var data = {
+        "operation": _self.operation,
+        'type': _self.myaction,
+        "action": _self.ajaxponit,
+        'data': 'init'
+    };
+
+    var init_xhr = {
+        type: 'post',
+        async: true,
+        url: ajaxurl,
+        dataType: 'HTML',
+        data: data,
+        success: function(response) {
+            jQuery('.metahtml').html(response);
+        },
+        error: function(response) {
+                
+        }
+    };
+
+    jQuery.ajax(init_xhr);
 };
 
 Traces.prototype.add = function() {
