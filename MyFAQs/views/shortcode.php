701fcd336a20220beb0784ae6ce3224218406869
huimingdeng <13570313864@163.com>
2019-04-11 10:09:28 +0800

    修改短代码模板

--- MyFAQs/views/shortcode.php
+++ MyFAQs/views/shortcode.php
@@ -1,28 +1,25 @@
-<link rel="stylesheet" href="<?php echo dirname(plugin_dir_url(__FILE__)). '/assets/css/bootstrap.min.css';?>" type="text/css">
-<h1>hello</h1>
+<style type="text/css">
+	.wrap ul li ul{
+		list-style: circle;
+		margin-left: 0px;
+		padding-left: 30px;
+		display: none;
+	}
+</style>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$('.faq-toggle').click(function(event) {
+			$(this).siblings('ul').toggle();
+		});
+	});
+</script>
 <div class="wrap">
-	<?php 
-	 ?>
-	<table class="table table-striped table-responsive" id="myfaqs-<?php echo $atts['class'];?>" title="<?php echo $atts['title']; ?>">
-		<thead>
-	        <tr>
-	            <th ><input type="checkbox" name="select"></th>
-	            <th width="15%"><?php _e('Title','myfaqs');?></th>
-	            <th width="40%"><?php _e('Answer', 'myfaqs');?></th>
-	            <th><?php _e('Category', 'myfaqs');?></th>
-	            <th><?php _e('EditDate', 'myfaqs');?></th>
-	            <th><?php _e('Action', 'myfaqs'); ?></th>
-	        </tr>
-        </thead>
-        <tbody>
-        	<tr>
-        		<td></td>
-        		<td><?php echo $faq['title']; ?></td>
-        		<td><?php echo $faq['answer']; ?></td>
-        		<td><?php echo $faq['category']; ?></td>
-        		<td><?php echo $faq['editdate']; ?></td>
-        		<td><?php echo $faq['id']; ?></td>
-        	</tr>
-        </tbody>
-	</table>
+	<ul>
+		<li>
+			<a href="javascript:void(0);" class="faq-toggle"><?php echo $faq['title']; ?></a>
+			<ul>
+				<li><?php echo $faq['answer']; ?></li>
+			</ul>
+		</li>
+	</ul>
 </div>
